d0c14a4f1f844938b606895c5cd78752d077987f
justin <justin@163.com>
2025-07-23 10:21:24 +0800

    simple configure

--- ghscripts/publish.ts
+++ ghscripts/publish.ts
@@ -1,10 +1,13 @@
-import { get_build_dir } from "./utils";
 import shell from 'shelljs';
+import fs from "fs";
+
+let cwd = process.cwd();
+let sync_files = ["package.json"];
 
 function publishPackage() {
     let build_dir = get_build_dir();
 
-    let cmd = `npm publish`;
+    let cmd = `npm publish --registry https://npm.pkg.github.com`;
 
     console.log(cmd);
 
@@ -12,4 +15,36 @@ function publishPackage() {
     shell.exec(cmd, { fatal: true })
 }
 
-publishPackage();
+function syncPackageJson() {
+    console.log("run packages");
+    console.log(cwd);
+ 
+    let ghscripts_path = `${cwd}/ghscripts`;
+
+    let build_dir = get_build_dir();
+
+    sync_files.forEach(function(name) {
+        let from = `${ghscripts_path}/${name}`;
+        let to = `${build_dir}/${name}`;
+
+        console.log(`copy ${from} to ${to}`);
+
+        fs.copyFileSync(from ,to);
+    });
+}
+
+function get_build_dir() {
+    let cwd = process.cwd();
+
+    let build_path = `${cwd}/build`;
+    let build_dirs = fs.readdirSync(build_path);
+    let build_dir = build_dirs[0];
+    return `${build_path}/${build_dir}`;
+}
+
+function main() {
+    syncPackageJson();
+    publishPackage();
+}
+
+main();
