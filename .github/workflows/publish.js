f945c8303ae55c3aeffc3c95722e241f11b27710
justin <justin@163.com>
2025-08-02 16:15:27 +0800

    update

--- .github/workflows/publish.js
+++ .github/workflows/publish.js
@@ -219,8 +219,8 @@ function runGh() {
 
 async function main() {
     // runGh();
-    console.log(process.env)
-    console.log(process.env.GITHUB_RUN_ID)
+    // console.log(process.env)
+    // console.log(process.env.GITHUB_RUN_ID)
     setup();
     let version = await getPackageVersion();
     writeVersion(version);
